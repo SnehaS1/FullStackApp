302be21fb5c951f328d6260482209f5f0b099c0f
Sneha S <snehasushatha@gmail.com>
2024-03-25 14:45:52 +0400

    client

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+import AboutPage from "./pages/about";
+import Sidebar from "./components/sidebar";
+import VehiclePage from "./pages/vehicles";
+import apiClient from "./http-common";
+import TrackVehiclePage from "./pages/track";
+import MaintenancePage from "./pages/maintenance";
+import UpsertMaintenancePage from "./pages/upsertMaintenance";
+import HomePage from "./pages/home";
+import UpsertVehiclePage from "./pages/upsertVehicle";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      {/* <Sidebar/> */}
+      <div className="conatineVehicle">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/vehicle" element={<VehiclePage />} />
+          <Route path="/vehicle/:id" element={<UpsertVehiclePage />} />
+          <Route path="/track" element={<TrackVehiclePage />} />
+          <Route path="/maintenance" element={<MaintenancePage />} />
+          <Route path="/maintenance/:id" element={<UpsertMaintenancePage />} />
+
+          {/* <Route path="/about" element={<About />} /> */}
+        </Routes>
+      </div>
+      {/* <Sidebar /> */}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import {
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import "./styles.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
